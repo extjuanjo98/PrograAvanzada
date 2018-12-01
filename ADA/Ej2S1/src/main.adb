b799573c7ff0a7e923e0e2915f51fedcb87d8cde
Juanjo Salvo <44440124+extjuanjo98@users.noreply.github.com>
2018-12-01 16:16:07 +0100

    Add files via upload

--- ADA/Ej2S1/src/main.adb
+++ ADA/Ej2S1/src/main.adb
@@ -7,16 +7,11 @@ procedure Main is
    package Entero_IO is new Text_IO.Integer_IO(Integer);
 
 begin
-   --  Insert code here.
-   --while (nat<0)and(nat>10) loop
-    --  while nat>10 loop
          Text_IO.Put_Line("Introduce  un natural:");
          Entero_IO.Get(nat);
          Text_IO.New_Line;
          Text_IO.Put("El Natural leido: ");
          Entero_IO.Put(nat);
          Text_IO.New_Line;
-    --  end loop;
-   --end loop;
 
 end Main;
